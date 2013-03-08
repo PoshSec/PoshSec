8630d76a1fec27a500f6097a91a14f1e9e4b20cc
J Wolfgang Goerlich <jwgoerlich@gmail.com>
2013-03-08 14:34:54 -0500

    Initial commit of PoshSec.PowerShell.Commands.dll

---
+++ PoshSec.PowerShell.Commands/PoshSec.PowerShell.Commands/PowerShell/Nouns.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="Nouns.cs" company="PoshSec (https://github.com/PoshSec/)">
+//     Copyright © 2013 and distributed under the BSD license.
+// </copyright>
+
+namespace PoshSec.PowerShell
+{
+    using System;
+
+    /// <summary>
+    /// Defines the common noun names that can be used to name PoshSec cmdlets.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Scope = "module", Justification = "Reviewed. The element names are self-explanatory.")]
+    internal static class Nouns
+    {
+        public const string PoshSecHello = "PoshSecHello";
+    }
+}
