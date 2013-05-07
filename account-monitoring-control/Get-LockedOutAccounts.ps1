b668065641553d674b91bb1e7f567050f62295b5
Matt Johnson <mwjcomputing@gmail.com>
2013-05-07 10:23:32 -0400

    Finalize Get-LockedOutAccounts
    Finalize Get-LockedOutAccounts

---
+++ account-monitoring-control/Get-LockedOutAccounts.ps1
@@ -0,0 +1,14 @@
+﻿function Get-LockedOutAccounts {
+
+    $adsiSearcher = New-Object DirectoryServices.DirectorySearcher("LDAP://rootdse")
+    $adsiSearcher.filter = "ObjectCategory=User"
+    
+    $adsiSearcher.findAll() | 
+    ForEach-Object {
+        If(([adsi]$_.path).psbase.invokeGet("IsAccountLocked")) {
+            $list = $list + ([adsi]$_.path).DistinguishedName
+        }
+    } 
+
+    Write-Output $list
+}
