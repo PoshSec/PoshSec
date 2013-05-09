32217d6ed73bc4c71d55bbec629437e358e43bb9
mwjcomputing <mwjcomputing@gmail.com>
2013-05-09 14:26:30 -0400

    Add Confirm-IsAdministrator
    Checks to see if user is administrator.

---
+++ utility-functions/Confirm-IsAdministrator.ps1
@@ -0,0 +1,6 @@
+﻿ function Confirm-IsAdministrator {
+    if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){
+        Write-Warning "You do not have Administrator rights to run this script! Please re-run this script as an Administrator!"
+        Break
+    }
+}
