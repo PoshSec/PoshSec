48965089f49d707f991abc31254af644edbcde94
NJJacob <njacob2@emich.edu>
2013-05-15 16:43:40 -0300

    Update Get-SecInventory.ps1

--- Inventory of Authorized and Unauthorized Devices/Get-SecInventory.ps1
+++ Inventory of Authorized and Unauthorized Devices/Get-SecInventory.ps1
@@ -1,13 +1,16 @@
 ﻿function Get-SecInventory {
 
 <#
--Description Get-SecInventory -Synopsis "Retrieves all computer accounts for a single domain"
+-Description Get-SecInventory 
+-Synopsis "Retrieves all computer accounts for a single domain"
+
+An array was an easier way of both collecting results and outputting them.
+The strCategory was unnecessary, since it was only called in one location. It was removed to avoid redunancy
+
 #>
 
 $list=@()
-#an array was an easier way of both collecting results and outputting them
 
-#the strCategory was unnecessary, since it was only called in one location. It was removed to avoid redunancy
 
 $objDomain = New-Object System.DirectoryServices.DirectorySearcher("LDAP://rootdse")
 
@@ -18,7 +21,6 @@ $objSearcher.Filter = "objectCategory=computer"
 $objSearcher.FindAll() | Foreach {
     $list = $list + ([adsi]$_.path).DistinguishedName
 }
-#Again, it was easier to place the results in an array 
 
 Write-Output $list
     
