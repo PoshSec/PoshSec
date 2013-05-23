b26bd52b234f5f50a95820ea7d35e9d3663a6e6f
NJJacob <njacob2@emich.edu>
2013-05-23 09:38:08 -0300

    Update and rename Get-SecInventory.ps1 to Get-SecDeviceInventory.ps1

---
+++ Inventory of Authorized and Unauthorized Devices/Get-SecDeviceInventory.ps1
@@ -0,0 +1,31 @@
+function Get-SecDeviceInventory 
+{
+
+$list = @()
+ 
+$objSearcher = New-Object DirectoryServices.DirectorySearcher("LDAP://rootdse")
+$objSearcher.SearchRoot = $objDomain
+$objSearcher.Filter = "objectCategory=computer"
+$objSearcher.FindAll() | Foreach {
+    $list += ([adsi]$_.path).DistinguishedName
+}
+ 
+#Write-Output $list
+ 
+if(-NOT(Test-Path ".\Device-Inventory-Baseline.xml"))
+    {
+		Write-Output $list | Export-Clixml "Device-Inventory-Baseline.xml"
+		Write-Warning "Baseline list now created. Please re-run this script to check for unauthorized devices."
+	
+	}
+
+[System.Array]$authorized = Import-Clixml -Path ".\Device-Inventory-Baseline.xml"
+
+[string]$exception = Get-DateISO8601 -Prefix ".\Device-Exception-Report" -Suffix ".xml"
+Compare-Object $list $authorized | Export-Clixml ".\Exception-Reports\$exception"
+
+    # The script can be emailed for review or processing in the ticketing system:
+	# Send-MailMessage -To -Subject "Device Inventory Exception" -Body "The report is attached." -Attachments $exception
+
+
+}
