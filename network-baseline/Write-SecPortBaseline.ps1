b3933f5a24a84bee0af64ca169da819229e858be
NJJacob <njacob2@emich.edu>
2013-05-23 11:26:36 -0300

    Update Write-SecPortBaseline.ps1

--- network-baseline/Write-SecPortBaseline.ps1
+++ network-baseline/Write-SecPortBaseline.ps1
@@ -3,8 +3,7 @@
       [string]$computer = Get-Content env:ComputerName
       [string]$filename = ".\$computer-Ports-Baseline.xml"
 
-      if (-not ($computer[0] -eq 'localhost') -or $computer.Length -gt 1 )
-       { 
+      
         
        $ipgp = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties();
        $listens = $ipgp.GetActiveTcpListeners();
@@ -13,30 +12,8 @@
        $table += $computer, $ip.Port   
           
        }
-        Write-Output $table | Export-Clixml -Path $filename 
-         
-         #Out-File -filepath $filename -Append -NoClobber -inputobject $table
- 
-
-          }
-          #local-only if no remoting
-          
-          
-        <#
-         else
-         {
-         
-          $ipgp = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties();
-          $listens = $ipgp.GetActiveTcpListeners();
-          foreach($ip in $listens)
-          {
-           $row = "localhost," + $ip.Port        
-           #Out-File -filepath $filename -Append -NoClobber -inputobject $row
-           Write-Output $row | Export-Clixml -Path $filename 
-          }
-          
-
-         }
-          #>
        
+       Write-Output $table | Export-Clixml -Path $filename 
+         
+              
 }
