d131dceb03001811e0224ab088acc36adfaae8bc
NJJacob <njacob2@emich.edu>
2013-05-23 10:34:36 -0300

    Update Get-SecSoftwareIntegrity.ps1

--- PoshSec.PowerShell.Commands/PoshSec.PowerShell.Commands/Scripts/Get-SecSoftwareIntegrity.ps1
+++ PoshSec.PowerShell.Commands/PoshSec.PowerShell.Commands/Scripts/Get-SecSoftwareIntegrity.ps1
@@ -1,4 +1,4 @@
-﻿function Get-SecSoftwareIntegrity
+function Get-SecSoftwareIntegrity
 {
     <#
     .Synopsis
@@ -13,17 +13,18 @@
     .Link
         https://github.com/organizations/PoshSec
     #>
-	
+
 	[string]$computer = Get-Content env:ComputerName
 	[string]$filename = Get-DateISO8601 -Prefix ".\$computer-Integrity" -Suffix ".xml"
-	Get-PoshFileIntegrity | Export-Clixml -Path $filename
+	Get-SecFileIntegrity | Export-Clixml -Path $filename
+    
+    if(-NOT(Test-Path ".\$computer-Integrity-Baseline.xml"))
+    {
+	    Rename-Item $filename "$computer-Integrity-Baseline.xml"
+   	    Write-Warning "The baseline file for this computer has been created, please run this script again."
+        Invoke-Expression $MyInvocation.MyCommand
+	    
+    }
+
 	
-	[System.Array]$approved = Import-Clixml -Path ".\$computer-Integrity-Baseline.xml"
-	[System.Array]$installed = Import-Clixml -Path $filename
-	
-	[string]$filename = Get-DateISO8601 -Prefix ".\$computer-Integrity-Exception-Report" -Suffix ".xml"
-	Compare-Object $approved $installed | Export-Clixml -Path $filename
-	
-	# The script can be emailed for review or processing in the ticketing system:
-	# Send-MailMessage -To -Subject "Installed software exception for $computer" -Body "The report is attached." -Attachments $filename
 }
