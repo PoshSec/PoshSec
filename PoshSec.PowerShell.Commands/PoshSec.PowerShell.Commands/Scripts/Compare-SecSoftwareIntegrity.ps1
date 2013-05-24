5d690d292fffe71db119592f5f8ca97750da54c1
NJJacob <njacob2@emich.edu>
2013-05-24 16:17:09 -0300

    Update Compare-SecSoftwareIntegrity.ps1

--- PoshSec.PowerShell.Commands/PoshSec.PowerShell.Commands/Scripts/Compare-SecSoftwareIntegrity.ps1
+++ PoshSec.PowerShell.Commands/PoshSec.PowerShell.Commands/Scripts/Compare-SecSoftwareIntegrity.ps1
@@ -21,7 +21,7 @@ function Compare-SecSoftwareIntegrity
 	[System.Array]$installed = Import-Clixml -Path $filename
 
 	[string]$filename = Get-DateISO8601 -Prefix ".\$computer-Integrity-Exception-Report" -Suffix ".xml"
-	Compare-Object $approved $installed | Export-Clixml ".\Exception-Reports\$filename"
+	Compare-Object $approved $installed | Export-Clixml ".\$filename"
 
 	# The script can be emailed for review or processing in the ticketing system:
 	# Send-MailMessage -To -Subject "Installed software exception for $computer" -Body "The report is attached." -Attachments $filename
