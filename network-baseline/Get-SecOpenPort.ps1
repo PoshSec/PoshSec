81e6371a3ec396086c37a70e5bd83299bbac8b0b
NJJacob <njacob2@emich.edu>
2013-05-24 16:01:11 -0300

    Update Get-SecOpenPort.ps1

--- network-baseline/Get-SecOpenPort.ps1
+++ network-baseline/Get-SecOpenPort.ps1
@@ -1,20 +1,30 @@
  function Get-SecOpenPort 
  {
     <#
-    Synopsis
+    .Synopsis
     Checks computer for open ports, then exports into an xml file. If a baseline does not exist, establishes baseline
+    
+    .Description
+        Baselines the open ports to an XML file.
+        
+        CSIS 20 Critical Security Controls for Effective Cyber Defense excerpt:
+ 	Limitation and control of network ports, protocols and services
+    .Example
+        Get-InstalledSoftware
+    .Link
+        https://github.com/organizations/PoshSec
     #>
+   
+      
 
       [string]$computer = Get-Content env:ComputerName
       [string]$filename = Get-DateISO8601 -Prefix ".\$computer-Ports" -Suffix ".xml"
-
-      
-        
+     
        $ipgp = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties();
        $listens = $ipgp.GetActiveTcpListeners();
        foreach($ip in $listens)
        {
-
+    
        [Array]$table += $ip.Port
           
        }
