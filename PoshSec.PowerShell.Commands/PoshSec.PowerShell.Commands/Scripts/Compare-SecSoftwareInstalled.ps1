842bb1fc187dae2671c5730bc4dcb47539a06fc2
NJJacob <njacob2@emich.edu>
2013-05-24 16:15:30 -0300

    Update Compare-SecSoftwareInstalled.ps1

--- PoshSec.PowerShell.Commands/PoshSec.PowerShell.Commands/Scripts/Compare-SecSoftwareInstalled.ps1
+++ PoshSec.PowerShell.Commands/PoshSec.PowerShell.Commands/Scripts/Compare-SecSoftwareInstalled.ps1
@@ -21,7 +21,7 @@
     [System.Array]$installed = Import-Clixml -Path $filename
 
     [string]$filename = Get-DateISO8601 -Prefix ".\$computer-Installed-Exception-Report" -Suffix ".xml"
-    Compare-Object $approved $installed | Export-Clixml  ".\Exception-Reports\$filename"
+    Compare-Object $approved $installed | Export-Clixml  ".\$filename"
 
     # The script can be emailed for review or processing in the ticketing system:
     # Send-MailMessage -To -Subject "Installed software exception for $computer" -Body "The report is attached." -Attachments $filename
