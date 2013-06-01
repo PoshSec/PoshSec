ab7f17793d8582ac94f1e2ced0f2b21f56b0c42a
mwjcomputing <mwjcomputing@gmail.com>
2013-05-31 22:45:35 -0400

    Add Authorized Devices to Main Module
    Add Authorized Devices to Main Module

--- poshsec.psm1
+++ poshsec.psm1
@@ -1,5 +1,6 @@
 ﻿Import-Module .\PoshSec.PowerShell.Commands\PoshSec.PowerShell.Commands\bin\Debug\PoshSec.PowerShell.Commands.dll
 Import-Module .\Account-Monitoring-Control\account-monitoring-control.psm1
+Import-Module .\Authorized-Devices\Authorized-Devices.psm1
 Import-Module .\Utility-Functions\Utility-Functions.psm1
 Import-Module .\Software-Management\Software-Management.psm1
 Import-Module .\Log-Management\Log-Management.psm1
