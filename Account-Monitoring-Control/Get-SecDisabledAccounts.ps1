9ae5d97ef1c51aee3c6a1575a76497d0edc100c5
NJJacob <njacob2@emich.edu>
2013-06-05 10:46:57 -0300

    Update Get-SecDisabledAccounts.ps1

--- Account-Monitoring-Control/Get-SecDisabledAccounts.ps1
+++ Account-Monitoring-Control/Get-SecDisabledAccounts.ps1
@@ -1,20 +1,12 @@
 ﻿function Get-SecDisabledAccounts {
 
-    $list = @()
-
-    $adsiSearcher = New-Object DirectoryServices.DirectorySearcher("LDAP://rootdse")
-    $adsiSearcher.filter = "(&(objectClass=user)(objectCategory=person))"
+    $filename = Get-DateISO8601 -Prefix "Disabled-Accounts" -Suffix ".xml"
     
-    $adsiSearcher.findAll() |
-    ForEach-Object {
-        $disabled = ([adsi]$_.path).psbase.invokeGet("useraccountcontrol")
-        if ($disabled -band 0x2) {
-            $list = $list + ([adsi]$_.path).DistinguishedName
-        }
+    Search-ADAccount -AccountDisabled | Export-Clixml $filename
+    [System.Array]$current = Import-Clixml $filename
+    [System.Array]$approved = Import-Clixml ".\Baselines\Disabled-Baseline.xml"
 
-    }
-
-    Write-Output $list
+    Compare-Object $approved $current | Export-Clixml ".\Exception-Reports\Disabled-Exception.xml"
 
     <#    
     .SYNOPSIS
