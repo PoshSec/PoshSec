cc00d795cadf83e3bcf94af4572e3ccd7c20e2aa
NJJacob <njacob2@emich.edu>
2013-06-06 15:30:34 -0300

    Create Get-SecFiles.ps1

---
+++ Software-Management/Get-SecFiles.ps1
@@ -0,0 +1,33 @@
+<#
+    Designed to search through the files and find any .dlls or .exes and then construct a list of those resources.
+    Calls itself recursively and allows it to search through all directories
+#>
+
+function Get-SecFiles
+{
+ 
+
+    [string]$computer = Get-Content env:ComputerName
+    [string]$filename = Get-DateISO8601 -Prefix ".\$computer-Files" -Suffix ".xml" 
+
+    [string]$searchParameters = Get-ChildItem $env:SystemRoot -Recurse -Include *.dll,*.exe
+ 
+    Write-Output $searchParameters | Export-Clixml $filename
+
+    
+    if(-NOT(Test-Path ".\Baselines\$computer-Files-Baseline.xml"))
+    {
+        Rename-Item $filename "$computer-Files-Baseline.xml"
+        Move-Item "$computer-Files-Baseline.xml" .\Baselines
+         Write-Warning "The baseline file for this computer has been created, now running the script again."
+        Invoke-Expression $MyInvocation.MyCommand
+	    
+    }
+
+    else
+    {
+        Compare-SecFiles
+    }
+      
+
+}
