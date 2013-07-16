527ca8d0e283214abdbc83b78d143e878c748a83
Matt Johnson <mwjcomputing@gmail.com>
2013-07-16 08:37:43 -0400

    Fixed Module Loading Issue
    Fixed Module Loading Issue

--- poshsec.psm1
+++ poshsec.psm1
@@ -1,8 +1,2 @@
-﻿Import-Module .\PoshSec.PowerShell.Commands\PoshSec.PowerShell.Commands\bin\Debug\PoshSec.PowerShell.Commands.dll
-Import-Module .\Account-Monitoring-Control\account-monitoring-control.psm1
-Import-Module .\Authorized-Devices\Authorized-Devices.psm1
-Import-Module .\Utility-Functions\Utility-Functions.psm1
-Import-Module .\Software-Management\Software-Management.psm1
-Import-Module .\Log-Management\Log-Management.psm1
-Import-Module .\Network-Baseline\Network-Baseline.psm1
-Import-Module .\Baselines\Baselines.psm1
+﻿Get-ChildItem $PSScriptRoot | ? { $_.PSIsContainer -and $_.Name -ne "PoshSec.PowerShell.Commands" } | % { Import-Module $_.FullName }
+Import-Module $PSScriptRoot\PoshSec.PowerShell.Commands\PoshSec.PowerShell.Commands\bin\Debug\PoshSec.PowerShell.Commands.dll
