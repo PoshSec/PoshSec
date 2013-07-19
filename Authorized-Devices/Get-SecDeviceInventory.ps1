00a0b01fa54187863d39bf008a6ec4deef7a071e
NJJacob <njacob2@emich.edu>
2013-07-19 13:43:44 -0400

    Update Get-SecDeviceInventory.ps1

--- Authorized-Devices/Get-SecDeviceInventory.ps1
+++ Authorized-Devices/Get-SecDeviceInventory.ps1
@@ -1,38 +1,29 @@
 function Get-SecDeviceInventory 
 {
 
+     <#
 
-<#
-
-    Synopsis
-    To generate a list of devices on the network and then export it to an XML document.
+        Synopsis
+        Control 1: Inventory of Authorized and Unauthorized Devices
+        Uses AD to generate a list of devices that are on the network, exports it to a XML document.
+        Checks to see if a baseline exists, and if one does not, renames the current report to the baseline, then runs itself again. Upon completion, calls sister-function
    
-#>
+    #>
 
-$list = @()
-
-[string]$filename = Get-DateISO8601 -Prefix ".\Device-Inventory" -Suffix ".xml" 
-
-$objSearcher = New-Object DirectoryServices.DirectorySearcher("LDAP://rootdse")
-$objSearcher.SearchRoot = $objDomain
-$objSearcher.Filter = "objectCategory=computer"
-$objSearcher.FindAll() | Foreach {
-    $list += ([adsi]$_.path).DistinguishedName
-}
+    [string]$filename = Get-DateISO8601 -Prefix ".\Device-Inventory" -Suffix ".xml"
+    Get-ADComputer -Filter * | Select-Object DistinguishedName,DNSHostName,Name,SAMAccountName,ObjectGUID | Export-Clixml $filename
  
-Write-Output $list | Export-Clixml $filename
-
-if(-NOT(Test-Path ".\Device-Inventory-Baseline.xml"))
-	{
-		Rename-Item $filename ".\Device-Inventory-Baseline.xml"
-		Write-Warning "Baseline list now created"
-	   	Invoke-Expression $MyInvocation.MyCommand
-	}
-else
-    {
-        Compare-SecDeviceList
-    }
-
-
-
+    if(-NOT(Test-Path ".\Baselines\Device-Inventory-Baseline.xml"))
+	    {
+		    Rename-Item $filename "Device-Inventory-Baseline.xml"
+            Move-Item ".\Device-Inventory-Baseline.xml" .\Baselines
+		    Write-Warning "Baseline list now created"
+	   	    Invoke-Expression $MyInvocation.MyCommand
+	    }
+    else
+        {
+            Compare-SecDeviceList
+        }
+        
 }
+
