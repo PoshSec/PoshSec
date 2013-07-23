f0566d2fd8537186897d3187b294802d461ca645
NJJacob <njacob2@emich.edu>
2013-07-23 09:03:05 -0400

    Update Compare-SecWirelessNetworks.ps1

--- Network-Baseline/Compare-SecWirelessNetworks.ps1
+++ Network-Baseline/Compare-SecWirelessNetworks.ps1
@@ -11,7 +11,7 @@ function Compare-SecWAPs
     Move-Item $filename .\Reports
 
     [string]$exception = Get-DateISO8601 -Prefix ".\$computer-WAP-Exception-Report" -Suffix ".xml"
-     Compare-Object -ReferenceObject $approved -DifferenceObject $installed -CaseSensitive | Export-Clixml  ".\Exception-Reports\$exception"
+    Compare-Object -ReferenceObject $approved -DifferenceObject $installed -CaseSensitive | Export-Clixml  ".\Exception-Reports\$exception"
 
     # The script can be emailed for review or processing in the ticketing system:
     # Send-MailMessage -To -Subject "Wireless access point exception for $computer" -Body "The report is attached." -Attachments $filename
