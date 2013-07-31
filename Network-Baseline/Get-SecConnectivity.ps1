05955c8ecdb9ba88815e82241e7feedfc94dcb44
NJJacob <njacob2@emich.edu>
2013-07-31 09:31:10 -0400

    Update Get-SecConnectivity.ps1

--- Network-Baseline/Get-SecConnectivity.ps1
+++ Network-Baseline/Get-SecConnectivity.ps1
@@ -3,6 +3,7 @@ function Get-SecConnectivity {
   Designed to check each device currently available on the network and see whether they are responsive.
   #>
  
+    $computer = Get-Content env:ComputerName 
     [array]$deviceNames = Get-ADComputer -Filter * | select -ExpandProperty 'Name'  
     Foreach($device in $deviceNames){
     if(Test-Connection $device -Count 1 -Quiet){
@@ -10,6 +11,8 @@ function Get-SecConnectivity {
         }
     else{
         Write-Host "$device is not connected"
+        # The script can be emailed for review or processing in the ticketing system:
+        # Send-MailMessage -To -Subject "Connectivity Alert" -Body "$computer cannot communicate with $device" 
         }
     } 
 } 
