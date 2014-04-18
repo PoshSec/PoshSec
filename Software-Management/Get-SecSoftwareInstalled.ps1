1d396082c6c8749bbd8678245373ebf17cb8c95d
NJJacob <njacob2@emich.edu>
2014-04-18 12:48:25 -0400

    Update Get-SecSoftwareInstalled.ps1

--- Software-Management/Get-SecSoftwareInstalled.ps1
+++ Software-Management/Get-SecSoftwareInstalled.ps1
@@ -1,18 +1,6 @@
 function Get-SecSoftwareInstalled
 {
-    <#
-    .Synopsis
-        Baselines the installed software to an XML file.
-    .Description
-        Baselines the installed software to an XML file.
-        
-        CSIS 20 Critical Security Controls for Effective Cyber Defense excerpt:
-		Devise a list of authorized software that is required in the enterprise for each type of system, including servers, workstations, and laptops of various kinds and uses. This list should be tied to file integrity checking software to validate that the software 
-    .Example
-        Get-InstalledSoftware
-    .Link
-        https://github.com/organizations/PoshSec
-    #>
+    
 	
 
     [string]$computer = Get-Content env:ComputerName
@@ -38,5 +26,17 @@ function Get-SecSoftwareInstalled
         Compare-SecSoftwareVersion
 
     }
-
+    <#
+    .Synopsis
+        Baselines the installed software to an XML file.
+    .Description
+        Baselines the installed software to an XML file.
+        
+        CSIS 20 Critical Security Controls for Effective Cyber Defense excerpt:
+		Devise a list of authorized software that is required in the enterprise for each type of system, including servers, workstations, and laptops of various kinds and uses. This list should be tied to file integrity checking software to validate that the software 
+    .Example
+        Get-InstalledSoftware
+    .Link
+        https://github.com/organizations/PoshSec
+    #>
 }
