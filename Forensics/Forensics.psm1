1bd67cffd6a7272d10b2a7a762b7883bae05945e
Matt Johnson <mwjcomputing@gmail.com>
2014-10-16 14:17:24 -0400

    Update to local SID enumerator
    Change name from Get-SID to Get-SecLocalSID
    Added Comment Based Help
    Corrected functionality

--- Forensics/Forensics.psm1
+++ Forensics/Forensics.psm1
@@ -10,3 +10,4 @@
 . (Join-Path $PSScriptRoot Get-SystemUptime.ps1)
 . (Join-Path $PSScriptRoot Get-RegistryHives.ps1)
 . (Join-Path $PSScriptRoot Get-SecADSid.ps1)
+. (Join-Path $PSScriptRoot Get-SecLocalSID.ps1)
