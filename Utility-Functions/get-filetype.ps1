3af66a286d9ce676d6392b45636484927dd90099
Rich Cassara <rjcassara@gmail.com>
2015-03-14 00:52:32 -0400

    adding documentation

--- Utility-Functions/get-filetype.ps1
+++ Utility-Functions/get-filetype.ps1
@@ -1,4 +1,30 @@
-﻿function Get-Filetype{
+﻿
+
+function Get-Filetype{
+<#
+.SYNOPSIS
+Takes an unknown file input and tries to determine what kind of file it is by inspecting the first 8 bytes
+.DESCRIPTION
+Compares the first 8 bytes of a file to a switch statement to determine file type.  Easily customizable to add new file headers (magic numbers) 
+
+.PARAMETER File
+The path to the file.
+.EXAMPLE
+PS C:\> Get-Filetype mysteryfile.???
+.INPUTS
+System.String
+.OUTPUTS
+String
+.NOTES
+AUTHOR: Rich Cassara
+This is a part of the PoshSec Utility-Functions module.
+.LINK
+www.poshsec.com
+.LINK
+github.com/poshsec
+#>
+
+
 [CmdletBinding()]
 param(
 [parameter(Mandatory=$true)]
