d1e294d93272c62b10030d99e4471ddce67d4a5f
Matt Johnson <mwjcomputing@gmail.com>
2015-03-15 20:45:35 -0400

    Update to Show-SecDisabledAccountAccess
    This update is to address the use of quotes inside the
    Show-SecDisabledAccountAccess function.

--- Account-Monitoring-Control/Show-SecDisabledAccountAccess.ps1
+++ Account-Monitoring-Control/Show-SecDisabledAccountAccess.ps1
@@ -7,7 +7,7 @@
     begin {
         if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
         {
-            Write-Warning "You do not have Administrator rights to run this script! Please re-run this script as an Administrator!"
+            Write-Warning 'You do not have Administrator rights to run this script! Please re-run this script as an Administrator!'
             Break
         }
     }
