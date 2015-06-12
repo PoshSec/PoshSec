7d8439aa14f42dd6e13ad15178f198cc742d7ddb
Matt Johnson <mjohnson@mwjcomputing.com>
2015-06-12 10:35:27 -0400

    Update PoshSec.Tests.ps1

--- Tests/PoshSec.Tests.ps1
+++ Tests/PoshSec.Tests.ps1
@@ -1,8 +1,8 @@
-Import-Module $PSScriptRoot\..\poshsec.psd1
+Import-Module $PSScriptRoot\..\poshsec.psd1 | Out-Null
 
 Describe 'PoshSec Module Load' {
   It 'There should be 64 commands.' {
     $Commands = @(Get-Command -Module 'PoshSec' | Select-Object -ExpandProperty Name)
-    $Commands.Count | Should Be 64
+    $Commands.Count | Should Be 69
   }
 }
