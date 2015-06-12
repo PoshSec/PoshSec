d2b03b8605d5d1f3c057b4a25599bb6f36d42d6c
Matt Johnson <mjohnson@mwjcomputing.com>
2015-06-12 10:07:08 -0400

    Update PoshSec.Tests.ps1

--- Tests/PoshSec.Tests.ps1
+++ Tests/PoshSec.Tests.ps1
@@ -1,5 +1,8 @@
+Import-Module $PSScriptRoot\..\poshsec.psd1
+
 Describe 'PoshSec Module Load' {
-  It 'Should load the PoshSec Module' {
-    Get-Module 'PoshSec' -Verbose
+  It 'There should be 64 commands.' {
+    $Commands = @(Get-Command -Module 'PoshSec' | Select-Object -ExpandProperty Name)
+    $Commands.Count | Should Be 64
   }
 }
