fda44a71a4cc2e5437782e52cb44f79035c001a3
Matt Johnson <mjohnson@mwjcomputing.com>
2015-06-15 15:10:32 -0400

    Update PoshSec.Tests.ps1
    Upped number of commands to 70.

--- Tests/PoshSec.Tests.ps1
+++ Tests/PoshSec.Tests.ps1
@@ -3,6 +3,6 @@ Import-Module $PSScriptRoot\..\poshsec.psd1 | Out-Null
 Describe 'PoshSec Module Load' {
   It 'There should be 64 commands.' {
     $Commands = @(Get-Command -Module 'PoshSec' | Select-Object -ExpandProperty Name)
-    $Commands.Count | Should Be 69
+    $Commands.Count | Should Be 70
   }
 }
