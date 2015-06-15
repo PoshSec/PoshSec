cb916ce5925e405cb81f930136ed134f0ca3e31a
Matt Johnson <mjohnson@mwjcomputing.com>
2015-06-15 14:50:52 -0400

    Fix psd1 file.

--- poshsec.psm1
+++ poshsec.psm1
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-﻿Get-ChildItem $PSScriptRoot | ? { $_.PSIsContainer -and $_.Name -ne 'PoshSec.PowerShell.Commands' -and $_.Name -ne 'PoshSec.PowerShell.Commands 3.5' } | % { Import-Module $_.FullName }
-=======
-﻿Get-ChildItem $PSScriptRoot | ? { $_.PSIsContainer -and $_.Name -ne "PoshSec.PowerShell.Commands" -and $_.Name -ne "PoshSec.PowerShell.Commands 3.5" -and $_.Name -ne "Tests" } | % { Import-Module $_.FullName }
->>>>>>> poshsec
+Get-ChildItem $PSScriptRoot | ? { $_.PSIsContainer -and $_.Name -ne "PoshSec.PowerShell.Commands" -and $_.Name -ne "PoshSec.PowerShell.Commands 3.5" -and $_.Name -ne "Tests" } | % { Import-Module $_.FullName }
 if ($PSVersionTable.PSVersion.Major -gt 2) {
     Import-Module $PSScriptRoot\PoshSec.PowerShell.Commands\PoshSec.PowerShell.Commands\bin\Debug\PoshSec.PowerShell.Commands.dll
 } else {
