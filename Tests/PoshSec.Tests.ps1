b73c960985b62720819a793e5adf2ff3430b124b
Matt Johnson <mwjcomputing@gmail.com>
2015-07-12 15:49:28 -0400

    Updated PoshSec Tests File
    Change feature count.

--- Tests/PoshSec.Tests.ps1
+++ Tests/PoshSec.Tests.ps1
@@ -1,8 +1,8 @@
 Import-Module $PSScriptRoot\..\poshsec.psd1 | Out-Null
 
 Describe 'PoshSec Module Load' {
-  It 'There should be 64 commands.' {
+  It 'There should be 56 commands.' {
     $Commands = @(Get-Command -Module 'PoshSec' | Select-Object -ExpandProperty Name)
-    $Commands.Count | Should Be 70
+    $Commands.Count | Should Be 56
   }
 }
