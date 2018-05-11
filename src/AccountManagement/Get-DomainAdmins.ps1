c5835722a1cc07f7b42bf8133fd1154d77e3e73e
Matt Johnson <mjohnson@mwjcomputing.com>
2018-05-11 19:48:01 -0400

    Updating Get-DomainAdmins
    Updaitng functionality to display correctly when grabbing domain admins.

--- src/AccountManagement/Get-DomainAdmins.ps1
+++ src/AccountManagement/Get-DomainAdmins.ps1
@@ -13,21 +13,33 @@ function Get-DomainAdmins {
     Part of the PoshSec PowerShell Module
     #>
 
-    $Entries = @()
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns','',Justification='Group name is Domain Admins')]
+    [CmdletBinding()]
+    param(
+        [Parameter(HelpMessage="Name of domain to run query against.")]
+        [string]$Domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+    )
 
-    $CurrentDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
-    $Domain = [ADSI]"LDAP://$CurrentDomain"
-    $DN = $Domain.DistinguishedName
+
+    #Domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+    $CurrentDomain = [ADSI]"LDAP://$Domain"
+    Write-Debug -Message "Set ADSI Connection to $CurrentDomain"
+    $DN = $CurrentDomain.DistinguishedName
+    Write-Debug -Message "Set Distinguished Name to $DN"
     $DomainAdmins = [ADSI]"LDAP://cn=Domain Admins,cn=Users,$DN"
+    Write-Debug -Message "Set Domain Admin string to $DomainAdmins"
 
-    Foreach($Member in $DomainAdmins) {
+    $ItemCount = $DomainAdmins.Count
+    #Write-Verbose -Message "Total number of members: $ItemCount"
+
+    Foreach($Member in $DomainAdmins.Member) {
         $MemberDN = [ADSI]"LDAP://$Member"
+        Write-Debug -Message "MemberDN: $MemberDN"
         $HashTable = [PSCustomObject]@{
             SamAccountName = $MemberDN.SamAccountName
             DisplayName = $MemberDN.DisplayName
         }
-        $Entries = $Entries + $HashTable
+        Write-Output $HashTable
     }
-
     Write-Verbose -Message "Displaying members of $DomainAdmins.SamAccountName"
 }
